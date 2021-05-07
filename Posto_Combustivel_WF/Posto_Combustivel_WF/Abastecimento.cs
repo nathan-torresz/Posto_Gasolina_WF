25ea5ea5da9be5621a280adf936a02071577c1e2
nathan-torresz <nathan_henrique2015@outlook.com>
2021-05-07 05:58:59 -0300

    Classe Abastecimento Concluída

--- Posto_Combustivel_WF/Posto_Combustivel_WF/Abastecimento.cs
+++ Posto_Combustivel_WF/Posto_Combustivel_WF/Abastecimento.cs
@@ -6,5 +6,33 @@ namespace Posto_Combustivel_WF
 {
     class Abastecimento
     {
+        private BombaCombustivel _bomba;
+        private float _qtd;
+        private float _preco;
+        private bool _isPago;
+        public BombaCombustivel Bomba { get => _bomba; private set => _bomba = value; }
+        public float Qtd
+        {
+            get => _qtd;
+            set
+            {
+                if (value < 0) _qtd = 0;
+            }
+        }
+        public float Preco { get => _preco; private set => _preco = value; }
+        public bool IsPago { get => _isPago; private set => _isPago = value; }
+        public float Valor { get => Qtd * Preco; }
+
+        public Abastecimento(BombaCombustivel bomba, float qtd, float preco)
+        {
+            Bomba = bomba;
+            Qtd = qtd;
+            Preco = preco;
+            this.IsPago = false;
+        }
+        public void MarcarPago(bool pago)
+        {
+            IsPago = pago;
+        }
     }
 }
--- Posto_Combustivel_WF/Posto_Combustivel_WF/BombaCombustivel.cs
+++ Posto_Combustivel_WF/Posto_Combustivel_WF/BombaCombustivel.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace Posto_Combustivel_WF
 {
+    public enum TipoCombustivel
+    {
+        Gasolina,
+        Etanol,
+    }
     class BombaCombustivel
     {
+        private int _numBomba;
+        private TipoCombustivel _tipo;
+        private float _capacidade;
+        public int NumBomba { get => _numBomba; private set => _numBomba = value; }
+        public TipoCombustivel Tipo { get => _tipo; private set => _tipo = value; }
+        public float Capacidade { get => _capacidade; set => _capacidade = value; }
+
+        public BombaCombustivel(int numBomba, TipoCombustivel tipo, float capacidade)
+        {
+            NumBomba = numBomba;
+            Tipo = tipo;
+            Capacidade = capacidade;
+        }
+
+        public Abastecimento Abastecer(float qtd, float preco)
+        {
+            if (qtd > Capacidade) qtd = Capacidade;
+            Capacidade -= qtd;
+            Abastecimento abastecimento = new Abastecimento(this, qtd, preco);
+            return abastecimento;
+        }
     }
 }
