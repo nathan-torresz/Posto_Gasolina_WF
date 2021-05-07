85435633f0166fd85265cd159e853990ca602365
nathan-torresz <nathan_henrique2015@outlook.com>
2021-05-07 06:07:12 -0300

    Avanço no codigo

--- Posto_Combustivel_WF/Posto_Combustivel_WF/PostoCombustivel.cs
+++ Posto_Combustivel_WF/Posto_Combustivel_WF/PostoCombustivel.cs
@@ -6,5 +6,63 @@ namespace Posto_Combustivel_WF
 {
     class PostoCombustivel
     {
+        private string _nome;
+        private List<BombaCombustivel> _bombas;
+        private List<Abastecimento> _abastecimento;
+        private float _precoGas;
+        private float _precoEtanol;
+
+        public string Nome { get => _nome; private set => _nome = value; }
+        public List<BombaCombustivel> Bombas { get => _bombas; private set => _bombas = value; }
+        public List<Abastecimento> Abastecimento { get => _abastecimento; private set => _abastecimento = value; }
+        public float PrecoGas { get => _precoGas; set => _precoGas = value; }
+        public float PrecoEtanol { get => _precoEtanol; set => _precoEtanol = value; }
+
+        public PostoCombustivel(string nome, TipoCombustivel[] tiposBombas, float precogas, float precoeta)
+        {
+            Nome = nome;
+            int numBomba = 1;
+            this.Bombas = new List<BombaCombustivel>();
+
+            foreach (TipoCombustivel tipo in tiposBombas)
+            {
+                BombaCombustivel b = new BombaCombustivel(numBomba++, tipo, 100f);
+                Bombas.Add(b);
+            }
+            Abastecimento = new List<Abastecimento>();
+            PrecoGas = precogas;
+            PrecoEtanol = precoeta;
+        }
+        public Abastecimento Abastecer(float qtd, int numBomba)
+        {
+            //Buscar a bomba pelo seu número
+            BombaCombustivel bomba = null;
+
+            foreach (BombaCombustivel b in Bombas)
+            {
+                if (b.NumBomba == numBomba) bomba = b;
+            }
+            if (bomba != null)
+            {
+                //buscar o tipo de combustível para saber o preço
+                float preco = 0;
+                switch (bomba.Tipo)
+                {
+                    case TipoCombustivel.Gasolina:
+                        preco = PrecoGas;
+                        break;
+                    case TipoCombustivel.Etanol:
+                        preco = PrecoEtanol;
+                        break;
+                }
+                Abastecimento abastecimento = bomba.Abastecer(qtd, preco);
+                Abastecimento.Add(abastecimento);
+                return abastecimento;
+            }
+            else
+            {
+                throw new Exception("Bomba não encontrada pelo número!");
+            }
+        }
     }
 }
--- Posto_Combustivel_WF/Posto_Combustivel_WF/Program.cs
+++ Posto_Combustivel_WF/Posto_Combustivel_WF/Program.cs
@@ -6,7 +6,65 @@ namespace Posto_Combustivel_WF
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PostoCombustivel posto1;
+            Abastecimento abastecimento1;
+
+            TipoCombustivel[] tiposBombas =
+            {
+                TipoCombustivel.Gasolina,
+                TipoCombustivel.Etanol,
+                TipoCombustivel.Gasolina,
+                TipoCombustivel.Etanol,
+            };
+            posto1 = new PostoCombustivel("Posto VGD", tiposBombas, 5.60f, 4.09f);
+
+            MostrarStatusBombas(posto1);
+
+            abastecimento1 = posto1.Abastecer(50, 2);
+            Abastecimento a2 = posto1.Abastecer(26, 2);
+            Abastecimento a3 = posto1.Abastecer(47, 4);
+            Abastecimento a4 = posto1.Abastecer(28, 3);
+            Abastecimento a5 = posto1.Abastecer(97.5f, 1);
+
+            abastecimento1.MarcarPago(true);
+            a2.MarcarPago(true);
+            a3.MarcarPago(true);
+            a4.MarcarPago(true);
+            a2.MarcarPago(true);
+
+            MudarPreco(posto1);
+
+            MostrarStatusBombas(posto1);
+        }
+        static void MostrarStatusBombas(PostoCombustivel posto)
+        {
+            Console.WriteLine($"Status das bombas do posto {posto.Nome}");
+            foreach (BombaCombustivel b in posto.Bombas)
+            {
+                Console.WriteLine($"Bomba {b.NumBomba} de {b.Tipo}\n" +
+                    $"Capacidade: {b.Capacidade}");
+            }
+            Console.WriteLine();
+        }
+        static void MudarPreco(PostoCombustivel posto)
+        {
+            TipoCombustivel tipo;
+            float preco;
+            int num = -1;
+            do
+            {
+                Console.WriteLine("Digite 0 para mudar o preço da gasolina.\n" +
+                    "Digite 1 para mudar o preço do etanol");
+                num = int.Parse(Console.ReadLine());
+            } while (num != 0 && num != 1);
+
+            if (num == 0) tipo = TipoCombustivel.Gasolina;
+            else tipo = TipoCombustivel.Etanol;
+
+            Console.WriteLine($"Digite o novo valor do combustível: {tipo}");
+            preco = float.Parse(Console.ReadLine());
+            if (tipo == TipoCombustivel.Gasolina) posto.PrecoGas = preco;
+            else if (tipo == TipoCombustivel.Etanol) posto.PrecoEtanol = preco;
         }
     }
 }
